23a6a88a488f68571fc85a9b5393de954fd055f7
Ajschaff <aschaff4@gmail.com>
2024-01-30 11:54:55 -0500

    feat: styling updates for mobile

--- src/components/HeroHistory/HeroHistory.tsx
+++ src/components/HeroHistory/HeroHistory.tsx
@@ -11,20 +11,22 @@ export interface HistoryProps {
 
 const HeroHistory: FC<HistoryProps> = ({ entries }) => {
   const theme = useTheme();
-  const matches = useMediaQuery(theme.breakpoints.down('sm'));
+  const smDown = useMediaQuery(theme.breakpoints.down('sm'));
 
   return (
     <Box sx={{ flexGrow: 1 }}>
       {entries.map((entry) => (
         <Grid key={entry.title} container>
-          <Grid container item xs sm={3} md={2} justifyContent={matches ? 'center' : 'initial'}>
+          <Grid container item xs sm={3} md={2} justifyContent={smDown ? 'center' : 'flex'}>
             <div className="rpgui-container framed mb-3 mr-3">
-              <Image src={entry.image} alt={entry.title} loading="lazy" width={75} height={75} />
+              <div style={{ borderRadius: '7px', overflow: 'hidden' }}>
+                <Image src={entry.image} alt={entry.title} loading="lazy" width={75} height={75} />
+              </div>
             </div>
           </Grid>
           <Grid item xs={12} sm={9} md={10} className="my-auto">
-            <h3 className={matches ? 'text-center' : 'text-left'}>{entry.title}</h3>
-            <h4 className={matches ? 'text-center' : 'text-left'}>{entry.timePeriod}</h4>
+            <h3 className={smDown ? 'text-center' : 'text-left'}>{entry.title}</h3>
+            <h4 className={smDown ? 'text-center' : 'text-left'}>{entry.timePeriod}</h4>
           </Grid>
           <Grid item xs={12} className="mb-6 rpgui-container framed">
             <ul className="rpgui-container framed-grey">
