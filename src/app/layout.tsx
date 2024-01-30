bb6b7667c862b9a5d88d47974aa1d6763c49fd30
Ajschaff <aschaff4@gmail.com>
2024-01-30 15:45:45 -0500

    fix: button widths

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import { Press_Start_2P } from 'next/font/google';
-import '@styles/rpgui.css';
+// import '@styles/rpgui.css';
 import '@styles/globals.css';
 
 const PS2P = Press_Start_2P({ subsets: ['latin'], weight: '400' });
