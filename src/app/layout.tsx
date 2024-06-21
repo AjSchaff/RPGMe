b410770ab8e7c20766d4c76d22b04c279e6ffc9e
Ajschaff <aschaff4@gmail.com>
2024-06-21 15:14:32 -0500

    fix: layout export failure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next';
 import Script from 'next/script';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
 import { AppProvider } from './AppContext';
-import { RPGUIProvider } from './RPGUIContext';
 import { ThemeProvider } from '@mui/material/styles';
 
 import { Press_Start_2P } from 'next/font/google';
@@ -10,7 +9,7 @@ import '@styles/globals.css';
 
 import { theme } from '../theme';
 
-export const PS2P = Press_Start_2P({ subsets: ['latin'], weight: '400' });
+const PS2P = Press_Start_2P({ subsets: ['latin'], weight: '400' });
 
 export const metadata: Metadata = {
   title: 'The Hero of Ages: Andrew Schaffner',
@@ -23,11 +22,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body className={PS2P.className}>
         <AppProvider>
-          <RPGUIProvider>
-            <AppRouterCacheProvider>
-              <ThemeProvider theme={theme}>{children}</ThemeProvider>
-            </AppRouterCacheProvider>
-          </RPGUIProvider>
+          <AppRouterCacheProvider>
+            <ThemeProvider theme={theme}>{children}</ThemeProvider>
+          </AppRouterCacheProvider>
         </AppProvider>
       </body>
 
